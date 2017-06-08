2e00b426e017538b20231bf15659254c0edf9629
Iurii Faevskii <urfaevskii@gmail.com>
2017-06-08 15:08:23 +0300

    Create test for comment box component

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -2,10 +2,10 @@ import {renderComponent,expect} from '../test_helper';
 import App from '../../src/components/app';
 
 describe('App',()=>{
+
   it('show the correct text', ()=>{
     const component = renderComponent(App);
-    
     expect(component).to.contain('React simple starter');
-
   });
+  
 });
